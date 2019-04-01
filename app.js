83789d10edb69f1fa0d0606ea6e156a8a0db2cd1
0030619 <a0030619@0030619s-MacBook-Pro.local>
2019-04-01 14:22:35 +0100

    precourse recap code

---
+++ app.js
@@ -0,0 +1,38 @@
+// Data Types & Variables
+  var myName = "Ben";
+  var myAge = 29;
+  let mySurname = "Sharp"
+  const DATABASE_NAME = "imdb"
+  console.log(myName + " " + mySurname);
+// Conditionals
+  var pinNumber = 1234
+
+  if (pinNumber === 1234){
+    console.log("Correct pin number");
+  }else{
+    console.log("Incorrect!");
+  }
+// Loops
+  var animals = ['cat','donkey','goose'];
+  for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+  }
+// Functions
+  var printName = function(){
+     console.log("Ben");
+  }
+
+  printName()
+  printName()
+  printName()
+  printName()
+// Arrays
+  var pubNames = ['Cloisters','4042','The Red Lion'];
+  console.log(pubNames);
+// Objects
+  var person = {
+    name: "Ben",
+    pinNumber: 1234,
+    tel: 999999
+  }
+  console.log(person);
